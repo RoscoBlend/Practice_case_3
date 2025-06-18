52a394f7b9d4d9c783922c632968fc9578fc5e92
RoscoBlend <145660466+RoscoBlend@users.noreply.github.com>
2025-06-18 12:43:47 +0900

    Update script.js

--- case_3/script.js
+++ case_3/script.js
@@ -1,4 +1,4 @@
-const images = ["images/1.jpg", "images/2.jpg", "images/3.jpg"];
+const images = ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg"];
 let current = 0;
 
 const imgElement = document.getElementById("slide");
